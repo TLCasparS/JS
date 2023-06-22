c612aeb1565ac0fed71b906bc4381121182e363a
TLCasparS <112706704+TLCasparS@users.noreply.github.com>
2023-06-22 08:53:11 +0200

    Add files via upload

---
+++ newletter/app.js
@@ -0,0 +1,68 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const request = require("request");
+const https = require("https");
+const mailchimp = require('@mailchimp/mailchimp_marketing');
+
+const API = "59b306feb73938bc93a9fe5cca63b376-us9";
+const listId = "129bd64f0a";
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+mailchimp.setConfig({
+  apiKey: API,
+  server: "us9"
+});
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/singup.html");
+
+});
+
+app.post("/", function(req, res){
+
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const mail = req.body.mail;
+
+  subscribingUser = {
+    firstName: req.body.fname,
+    lastName: req.body.lname,
+    email: req.body.mail
+  };
+  console.log(subscribingUser);
+  try {
+    run();
+    res.sendFile(__dirname + "/success.html");
+  } catch (error) {
+    console.log('Error:', error);
+    res.sendFile(__dirname + "/failure.html");
+  }
+
+});
+
+async function run() {
+  try {
+    const response = await mailchimp.lists.addListMember(listId, {
+      email_address: subscribingUser.email,
+      status: 'subscribed',
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName
+      }
+    });
+
+    console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}.`);
+  } catch (error) {
+    console.log('Error:', error);
+    throw error;
+  }
+}
+
+app.listen(3000, function(){
+    console.log("server running on port 3000")
+
+});
