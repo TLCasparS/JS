bcf822394b43553c356bb75f8c35bcfd83bdc0b7
TLCasparS <112706704+TLCasparS@users.noreply.github.com>
2023-06-23 16:10:34 +0200

    version 2
    added study todolist subpage /study

--- simple todolist/app.js
+++ simple todolist/app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 
 var days = ["sunday", "monday", "tuesday","wednesday", "thursday","friday","Saturday"];
+var months = ["Januray", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November","December"]
 var today = new Date();
 const options = {
     weekday: "long",
@@ -15,33 +16,55 @@ const options = {
     month: "long",
     day: "numeric",
   };
-  if (today.getDate() == 6 || today.getDate() == 0){
-    today = days[today.getDay()] +" the " +today.getDate()+ " it is Weekend";
-}else{
-    today = days[today.getDay()] +" the " +today.getDate()+ " it a is day";
-    
-}
+today = days[today.getDay()] +", " +today.getDate() +" " + months[today.getMonth()]; 
 var todo = [];
+var studytodo = []
 app.get("/", function(req, res){
    
-
+    var title = "General tasks"
     //var day = today.toLocaleDateString("de-DE", options);
    
    
-    res.render("list", {day: today, todo:todo});
+    res.render("list", {day: today, todo:todo, listTitle: title});
 
 });
 
 app.post("/", function(req, res){
+    
+    if (req.body.list === "study"){
+        studytodo.push(req.body.do);
+        res.redirect("/study");
+    }else{;
+     todo.push(req.body.do);
+     res.redirect("/");
+    }
+    
+    //console.log(todo);
+
+    //res.render("list", {todo:todo, day: today})
+    
+})
+
+
+app.get("/study", function(req, res){
+   
+
+    //var day = today.toLocaleDateString("de-DE", options);
+    var title= "study"
+   
+    res.render("list", {day: today, todo:studytodo, listTitle: title});
+
+});
+
+app.post("/study", function(req, res){
     
     todo.push(req.body.do);
     
     //console.log(todo);
 
     //res.render("list", {todo:todo, day: today})
-    res.redirect("/");
+    res.redirect("/study");
 })
-
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
